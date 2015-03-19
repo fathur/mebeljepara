0f97e242e66558d43bcd45d7f194c7bd269fab72
fathur <fathur_rohman17@yahoo.co.id>
2015-03-19 08:35:13 +0700

    init gh_pages

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/mebeljepara/admin/');
+define('HTTP_CATALOG', 'http://localhost/mebeljepara/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/mebeljepara/admin/');
+define('HTTPS_CATALOG', 'http://localhost/mebeljepara/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\www\mebeljepara/admin/');
+define('DIR_SYSTEM', 'D:\www\mebeljepara/system/');
+define('DIR_LANGUAGE', 'D:\www\mebeljepara/admin/language/');
+define('DIR_TEMPLATE', 'D:\www\mebeljepara/admin/view/template/');
+define('DIR_CONFIG', 'D:\www\mebeljepara/system/config/');
+define('DIR_IMAGE', 'D:\www\mebeljepara/image/');
+define('DIR_CACHE', 'D:\www\mebeljepara/system/cache/');
+define('DIR_DOWNLOAD', 'D:\www\mebeljepara/system/download/');
+define('DIR_UPLOAD', 'D:\www\mebeljepara/system/upload/');
+define('DIR_LOGS', 'D:\www\mebeljepara/system/logs/');
+define('DIR_MODIFICATION', 'D:\www\mebeljepara/system/modification/');
+define('DIR_CATALOG', 'D:\www\mebeljepara/catalog/');
+
+// DB
+define('DB_DRIVER', 'mpdo');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'mebeljepara');
+define('DB_PREFIX', 'mj_');
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/mebeljepara/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/mebeljepara/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\www\mebeljepara/catalog/');
+define('DIR_SYSTEM', 'D:\www\mebeljepara/system/');
+define('DIR_LANGUAGE', 'D:\www\mebeljepara/catalog/language/');
+define('DIR_TEMPLATE', 'D:\www\mebeljepara/catalog/view/theme/');
+define('DIR_CONFIG', 'D:\www\mebeljepara/system/config/');
+define('DIR_IMAGE', 'D:\www\mebeljepara/image/');
+define('DIR_CACHE', 'D:\www\mebeljepara/system/cache/');
+define('DIR_DOWNLOAD', 'D:\www\mebeljepara/system/download/');
+define('DIR_UPLOAD', 'D:\www\mebeljepara/system/upload/');
+define('DIR_MODIFICATION', 'D:\www\mebeljepara/system/modification/');
+define('DIR_LOGS', 'D:\www\mebeljepara/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mpdo');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'mebeljepara');
+define('DB_PREFIX', 'mj_');
