29b8fa36a8d6ce583f501d7546abff58ab56e0f4
fathur <fathur_rohman17@yahoo.co.id>
2015-03-19 08:30:49 +0700

    configuration env

--- README.md
+++ README.md
@@ -1,3 +1,92 @@
 # Mebel Jepara
 
 Website e-Commerce untuk Mebel Jepara berbasiskan OpenCart
+
+## Suped Duper, Theme Forest PSD:
+
+Color: carot and pumpkin
+
+Home:
+- home 3 ---->
+- home 1 with shadow color ---->
+
+Menu:
+- menu 1
+
+Account:
+
+http://forum.opencart.com/viewtopic.php?f=20&t=32872#p164179
+
+- ...
+
+All Category:
+- 04_all_categories_4
+
+Product\Category:
+- 05_catagory_1_raws
+- 05_catagory_1_table
+
+Product\Product:
+- 06 product_01_*
+- 06 product_quick_view_1
+
+Product\Compare:
+- 20_product_comparison_3
+
+Product\Search:
+- 33_search_advanced
+- 33_search_white
+
+
+Information\Information:
+- 08_article_experts_page
+
+Checkout/Cart:
+- shopping_cart_1
+- shopping_cart_4_1
+
+Checkout\Checkout:
+- 12_checkout
+
+Account/Register:
+- 10_register_account
+
+Account\Login:
+- 11_account_login
+- 11_account_login_2
+
+Account\Wishlist:
+- 21_my_account_wishlist
+
+Admin Login:
+- 11_account_login_3
+
+404:
+- 14_error_404_4
+
+Information/Contact:
+- 17_contact_us_2
+
+About Us
+- 18_about_3
+
+About Us - Team:
+- 18_about_4
+
+Profile
+- 21_my_account
+
+Coming-Soon/ Maintenance mode:
+- 28_coming_soon_2
+
+FAQ"
+- 36_faq-1
+
+Testimonial:
+- 35_testimonial_1
+
+Stock:
+- 34_stock_1
+
+Popup:
+- 24_popup_subscribe
--- catalog/view/theme/mebeljepara/gulpfile.bck.js
+++ catalog/view/theme/mebeljepara/gulpfile.bck.js
@@ -1,12 +1,13 @@
 var gulp          = require('gulp'),
+    browserSync   = require('browser-sync'),
     autoPrefixer  = require('gulp-autoprefixer'),
+    concat        = require('gulp-concat'),
     jade          = require('gulp-jade'),
     less          = require('gulp-less'),
     minifyCSS     = require('gulp-minify-css'),
     print         = require('gulp-print'),
     rename        = require('gulp-rename'),
-
-    
+    uglify        = require('gulp-uglify'),
 
     setPrefix = [
       'last 2 version',
@@ -28,8 +29,32 @@ var gulp          = require('gulp'),
         source: 'less/style.less',
         dest: 'css/'
       },
+      js: {
+        source: [
+          'bower_components/jquery/dist/jquery.js',
+          'bower_components/bootstrap/js/transition.js',
+          'bower_components/bootstrap/js/collapse.js'
+        ],
+        dest: 'js/'
+      },
+      maps: {
+        dest: 'maps/'
+      }
     }
 ;
+
+
+// BrowserSync
+gulp.task('browser-sync', function() {
+  browserSync({
+    proxy: 'localhost:4400'
+  });
+});
+
+gulp.task('bs-reload', function () {
+  browserSync .reload();
+});
+
 
 // Build
 gulp.task('build:template', function() {
@@ -38,6 +63,7 @@ gulp.task('build:template', function() {
     .pipe(gulp.dest(files.template.dest))
     .pipe(print(function (file) { return file + ' has successfully created.' }));
 
+  browserSync .reload();
 });
 
 gulp.task('build:less', function() {
@@ -46,11 +72,27 @@ gulp.task('build:less', function() {
     .pipe(autoPrefixer(setPrefix))
     .pipe(gulp.dest(files.less.dest))
     .pipe(print(function (file) { return file + ' has successfully created.' }))
+    .pipe(browserSync.reload({ stream: true }))
     .pipe(rename('style.min.css'))
     .pipe(minifyCSS())
     .pipe(gulp.dest(files.less.dest))
-    .pipe(print(function (file) { return file + ' has successfully created.' }));
+    .pipe(print(function (file) { return file + ' has successfully created.' }))
+    .pipe(browserSync.reload({ stream: true }));
 });
+
+gulp.task('build:js', function() {
+  gulp.src(files.js.source)
+    .pipe(concat('core.js'))
+    .pipe(gulp.dest(files.js.dest))
+    .pipe(print(function (file) { return file + ' has successfully created.' }))
+    .pipe(uglify())
+    .pipe(rename('core.min.js'))
+    .pipe(gulp.dest(files.js.dest))
+    .pipe(print(function (file) { return file + ' has successfully created.' }));
+
+  browserSync.reload();
+});
+
 
 // Watch files
 gulp.task('watch', function() {
@@ -65,5 +107,6 @@ gulp.task('watch', function() {
   });
 });
 
+
 // Default
-gulp.task('default', ['watch']);
+gulp.task('default', ['browser-sync', 'watch']);
